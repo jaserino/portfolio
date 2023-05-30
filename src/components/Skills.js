6c597d42167ec15cbad49cc56a02e07eb0a92400
jaserino <jaserino1@gmail.com>
2023-05-30 11:27:58 -0400

    Updated resume and skills

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -38,13 +38,6 @@ export const Skills = () => {
               >
                 <div className="item">
                   <img
-                    src="https://img.icons8.com/color/144/null/java-coffee-cup-logo--v1.png"
-                    alt="Java"
-                  />
-                  <h5>Java</h5>
-                </div>
-                <div className="item">
-                  <img
                     src="https://img.icons8.com/color/144/000000/javascript--v1.png"
                     alt="JS"
                   />
@@ -56,6 +49,13 @@ export const Skills = () => {
                     alt="React"
                   />
                   <h5>React</h5>
+                </div>
+                <div className="item">
+                  <img
+                    src="https://img.icons8.com/color/144/null/java-coffee-cup-logo--v1.png"
+                    alt="Java"
+                  />
+                  <h5>Java</h5>
                 </div>
                 <div className="item">
                   <img
